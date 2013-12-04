3f5f0418a1aa98246084fa60c754e9745c8d61bc
Fridrich Štrba <fridrich.strba@bluewin.ch>
2013-12-04 16:18:45 +0100

    adding ABWStyles.cpp/h

---
+++ src/lib/ABWStyles.h
@@ -0,0 +1,15 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * This file is part of the libabw project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef __ABWSTYLES_H__
+#define __ABWSTYLES_H__
+
+
+#endif // __ABWSTYLES_H__
+/* vim:set shiftwidth=2 softtabstop=2 expandtab: */
