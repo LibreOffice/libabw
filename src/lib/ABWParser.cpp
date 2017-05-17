LibreOffice/libabw

commit 442b4812cf058481e00c186ef738c990e1a50284

ofz#1025 stop parsing if XML error is encountered
xmlTextReader either does not continue processing the document if it
encounters an error or it can get into an infinite loop in some cases.
XML_PARSE_RECOVER does not help. So we just give up in case of error.

Change-Id: Ib5287a4032b2f9600bd389dcfb2e2cc6babf6ac8

src/lib/ABWParser.cpp
@@ -187,11 +187,12 @@ bool libabw::ABWParser::processXmlDocument(librevenge::RVNGInputStream *input)
   if (!input)
     return false;
 
-  std::shared_ptr<xmlTextReader> reader(xmlReaderForStream(input), xmlFreeTextReader);
+  ABWXMLErrorWatcher watcher;
+  std::shared_ptr<xmlTextReader> reader(xmlReaderForStream(input, &watcher), xmlFreeTextReader);
   if (!reader)
     return false;
   int ret = xmlTextReaderRead(reader.get());
-  while (1 == ret)
+  while (1 == ret && !watcher.isError())
   {
     processXmlNode(reader.get());
     ret = xmlTextReaderRead(reader.get());
@@ -199,7 +200,7 @@ bool libabw::ABWParser::processXmlDocument(librevenge::RVNGInputStream *input)
 
   if (m_collector)
     m_collector->endDocument();
-  return true;
+  return !watcher.isError();
 }
 
 void libabw::ABWParser::processXmlNode(xmlTextReaderPtr reader)

src/lib/ABWXMLHelper.cpp
@@ -46,11 +46,12 @@ extern "C" {
   }
 
 #ifdef DEBUG
-  static void abwxmlReaderErrorFunc(void *, const char *message, xmlParserSeverities severity, xmlTextReaderLocatorPtr)
+  static void abwxmlReaderErrorFunc(void *arg, const char *message, xmlParserSeverities severity, xmlTextReaderLocatorPtr)
 #else
-  static void abwxmlReaderErrorFunc(void *, const char *, xmlParserSeverities severity, xmlTextReaderLocatorPtr)
+  static void abwxmlReaderErrorFunc(void *arg, const char *, xmlParserSeverities severity, xmlTextReaderLocatorPtr)
 #endif
   {
+    const auto watcher = reinterpret_cast<ABWXMLErrorWatcher *>(arg);
     switch (severity)
     {
     case XML_PARSER_SEVERITY_VALIDITY_WARNING:
@@ -64,6 +65,8 @@ extern "C" {
       break;
     case XML_PARSER_SEVERITY_ERROR:
       ABW_DEBUG_MSG(("Found xml parser severity error %s\n", message));
+      if (watcher)
+        watcher->setError();
       break;
     default:
       break;
@@ -89,13 +92,28 @@ ABWXMLString::operator const char *() const
   return reinterpret_cast<const char *>(m_xml.get());
 }
 
+ABWXMLErrorWatcher::ABWXMLErrorWatcher()
+  : m_error(false)
+{
+}
+
+bool ABWXMLErrorWatcher::isError() const
+{
+  return m_error;
+}
+
+void ABWXMLErrorWatcher::setError()
+{
+  m_error = true;
+}
+
 // xmlTextReader helper function
 
-xmlTextReaderPtr xmlReaderForStream(librevenge::RVNGInputStream *input)
+xmlTextReaderPtr xmlReaderForStream(librevenge::RVNGInputStream *input, ABWXMLErrorWatcher *watcher)
 {
   xmlTextReaderPtr reader = xmlReaderForIO(abwxmlInputReadFunc, abwxmlInputCloseFunc, (void *)input, 0, 0,
                                            XML_PARSE_NOBLANKS|XML_PARSE_NOENT|XML_PARSE_NONET|XML_PARSE_RECOVER);
-  xmlTextReaderSetErrorHandler(reader, abwxmlReaderErrorFunc, 0);
+  xmlTextReaderSetErrorHandler(reader, abwxmlReaderErrorFunc, watcher);
   return reader;
 }
 

src/lib/ABWXMLHelper.h
@@ -34,10 +34,25 @@ private:
   std::shared_ptr<xmlChar> m_xml;
 };
 
+class ABWXMLErrorWatcher
+{
+  ABWXMLErrorWatcher(const ABWXMLErrorWatcher &) = delete;
+  ABWXMLErrorWatcher &operator=(const ABWXMLErrorWatcher &) = delete;
+
+public:
+  ABWXMLErrorWatcher();
+
+  bool isError() const;
+  void setError();
+
+private:
+  bool m_error;
+};
+
 // create an xmlTextReader pointer from a librevenge::RVNGInputStream pointer
 // needs to be freed using xmlTextReaderFree function.
 
-xmlTextReaderPtr xmlReaderForStream(librevenge::RVNGInputStream *input);
+xmlTextReaderPtr xmlReaderForStream(librevenge::RVNGInputStream *input, ABWXMLErrorWatcher *watcher = 0);
 
 } // namespace libabw
 

commit 2a17664d1ce065c75094a05ff29a4feded742aa7

improve check for stuck XML parser
This fixes regression in parsing some broken documents caused by commit
442b4812cf058481e00c186ef738c990e1a50284 .

Change-Id: Id4afc8b120c72cb77f27c71c3f1452e100f86faf

src/lib/ABWParser.cpp
@@ -166,12 +166,12 @@ bool libabw::ABWParser::processXmlDocument(librevenge::RVNGInputStream *input)
   if (!input)
     return false;
 
-  ABWXMLErrorWatcher watcher;
+  ABWXMLProgressWatcher watcher;
   auto reader(xmlReaderForStream(input, &watcher));
   if (!reader)
     return false;
   int ret = xmlTextReaderRead(reader.get());
-  while (1 == ret && !watcher.isError())
+  while (1 == ret && !watcher.isStuck())
   {
     processXmlNode(reader.get());
     ret = xmlTextReaderRead(reader.get());
@@ -179,7 +179,7 @@ bool libabw::ABWParser::processXmlDocument(librevenge::RVNGInputStream *input)
 
   if (m_collector)
     m_collector->endDocument();
-  return !watcher.isError();
+  return ret == 1 && !watcher.isStuck();
 }
 
 void libabw::ABWParser::processXmlNode(xmlTextReaderPtr reader)

src/lib/ABWXMLHelper.cpp
@@ -51,7 +51,7 @@ extern "C" {
   static void abwxmlReaderErrorFunc(void *arg, const char *, xmlParserSeverities severity, xmlTextReaderLocatorPtr)
 #endif
   {
-    const auto watcher = reinterpret_cast<ABWXMLErrorWatcher *>(arg);
+    const auto watcher = reinterpret_cast<ABWXMLProgressWatcher *>(arg);
     switch (severity)
     {
     case XML_PARSER_SEVERITY_VALIDITY_WARNING:
@@ -66,7 +66,7 @@ extern "C" {
     case XML_PARSER_SEVERITY_ERROR:
       ABW_DEBUG_MSG(("Found xml parser severity error %s\n", message));
       if (watcher)
-        watcher->setError();
+        watcher->signalError();
       break;
     default:
       break;
@@ -92,29 +92,50 @@ ABWXMLString::operator const char *() const
   return reinterpret_cast<const char *>(m_xml.get());
 }
 
-ABWXMLErrorWatcher::ABWXMLErrorWatcher()
-  : m_error(false)
+ABWXMLProgressWatcher::ABWXMLProgressWatcher()
+  : m_reader(nullptr)
+  , m_line(0)
+  , m_col(0)
+  , m_wasError(false)
+  , m_isStuck(false)
 {
 }
 
-bool ABWXMLErrorWatcher::isError() const
+void ABWXMLProgressWatcher::setReader(xmlTextReaderPtr reader)
 {
-  return m_error;
+  m_reader = reader;
 }
 
-void ABWXMLErrorWatcher::setError()
+bool ABWXMLProgressWatcher::isStuck() const
 {
-  m_error = true;
+  return m_isStuck;
+}
+
+void ABWXMLProgressWatcher::signalError()
+{
+  if (m_reader && !m_isStuck)
+  {
+    const int line = m_line;
+    const int col = m_col;
+    const bool checkStuck = m_wasError;
+    m_wasError = true;
+    m_line = xmlTextReaderGetParserLineNumber(m_reader);
+    m_col = xmlTextReaderGetParserColumnNumber(m_reader);
+    if (checkStuck)
+      m_isStuck = line == m_line && col == m_col;
+  }
 }
 
 // xmlTextReader helper function
 
-std::unique_ptr<xmlTextReader, void(*)(xmlTextReaderPtr)> xmlReaderForStream(librevenge::RVNGInputStream *input, ABWXMLErrorWatcher *watcher)
+std::unique_ptr<xmlTextReader, void(*)(xmlTextReaderPtr)> xmlReaderForStream(librevenge::RVNGInputStream *input, ABWXMLProgressWatcher *watcher)
 {
   std::unique_ptr<xmlTextReader, void(*)(xmlTextReaderPtr)> reader(
     xmlReaderForIO(abwxmlInputReadFunc, abwxmlInputCloseFunc, (void *)input, 0, 0,
                    XML_PARSE_NOBLANKS|XML_PARSE_NOENT|XML_PARSE_NONET|XML_PARSE_RECOVER),
     xmlFreeTextReader);
+  if (watcher)
+    watcher->setReader(reader.get());
   if (reader)
     xmlTextReaderSetErrorHandler(reader.get(), abwxmlReaderErrorFunc, watcher);
   return reader;

src/lib/ABWXMLHelper.h
@@ -34,23 +34,29 @@ private:
   std::shared_ptr<xmlChar> m_xml;
 };
 
-class ABWXMLErrorWatcher
+class ABWXMLProgressWatcher
 {
-  ABWXMLErrorWatcher(const ABWXMLErrorWatcher &) = delete;
-  ABWXMLErrorWatcher &operator=(const ABWXMLErrorWatcher &) = delete;
+  ABWXMLProgressWatcher(const ABWXMLProgressWatcher &) = delete;
+  ABWXMLProgressWatcher &operator=(const ABWXMLProgressWatcher &) = delete;
 
 public:
-  ABWXMLErrorWatcher();
+  ABWXMLProgressWatcher();
 
-  bool isError() const;
-  void setError();
+  void setReader(xmlTextReaderPtr reader);
+
+  bool isStuck() const;
+  void signalError();
 
 private:
-  bool m_error;
+  xmlTextReaderPtr m_reader;
+  int m_line;
+  int m_col;
+  bool m_wasError;
+  bool m_isStuck;
 };
 
 // create an xmlTextReader pointer from a librevenge::RVNGInputStream pointer
-std::unique_ptr<xmlTextReader, void(*)(xmlTextReaderPtr)> xmlReaderForStream(librevenge::RVNGInputStream *input, ABWXMLErrorWatcher *watcher = 0);
+std::unique_ptr<xmlTextReader, void(*)(xmlTextReaderPtr)> xmlReaderForStream(librevenge::RVNGInputStream *input, ABWXMLProgressWatcher *watcher = 0);
 
 } // namespace libabw
 